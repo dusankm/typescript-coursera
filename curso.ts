f598112c12334d49c1054623f2aa16845f103057
dusankm <dusan43@hotmail.com>
2021-06-06 18:12:39 -0400

    tarea 6

--- curso.ts
+++ curso.ts
@@ -1,7 +1,7 @@
 
 export class Curso{
 
-    constructor(public nombre: string, public horas: number, public calificacion:number,public certificado:boolean,public año:number){
+    constructor(public nombre: string, public horas: number, public calificacion:number,public certificado:boolean,public anio:number){
 
     }
 }
--- scripts/curso.js
+++ scripts/curso.js
@@ -1,10 +1,10 @@
 var Curso = /** @class */ (function () {
-    function Curso(nombre, horas, calificacion, certificado, año) {
+    function Curso(nombre, horas, calificacion, certificado, anio) {
         this.nombre = nombre;
         this.horas = horas;
         this.calificacion = calificacion;
         this.certificado = certificado;
-        this.año = año;
+        this.anio = anio;
     }
     return Curso;
 }());
