a8bcafb53bc0f2a54bc15cf06d75552fd6639c9a
dusankm <dusankm95@gmail.com>
2021-06-06 18:19:18 +0000

    tarea 3

--- main.ts
+++ main.ts
@@ -1,1 +1,5 @@
-console.log('hola mundo');
+import{Aprendiz,NivelEducativo} from'./aprendiz.js'
+
+export const ap = new Aprendiz("Clara Andrea","Reyes Gomez","avatar.png",30,NivelEducativo.POSGRADO);
+ 
+console.log(ap);
